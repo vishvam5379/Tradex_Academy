ac37c60f6daca1ef51dc37de7217a00e0b4b8242
vishvam5379 <your-email@example.com>
2026-09-20 20:58:43 +0530

    Switch to CompressedStaticFilesStorage to fix staticfiles manifest missing entry 500 error

--- academy_core/settings.py
+++ academy_core/settings.py
@@ -215,13 +215,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
 
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
